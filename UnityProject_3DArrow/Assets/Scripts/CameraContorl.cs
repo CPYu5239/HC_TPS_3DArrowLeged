b2125faf150c758b6b5bda93a2932b2880bf4c6a
CPYu5239 <57282801+CPYu5239@users.noreply.github.com>
2019-12-21 21:03:13 +0800

    完成開門的動畫，攝影機的追蹤控制

---
+++ UnityProject_3DArrow/Assets/Scripts/CameraContorl.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class CameraContorl : MonoBehaviour
+{
+    [Header("速度"), Range(0, 100)]
+    public float speed = 1.5f;
+    [Header("上方限制")]
+    public float top = -21.4f;
+    [Header("下方限制")]
+    public float bottom = -18.8f;
+
+    private Transform player;
+
+    private void Start()
+    {
+        player = GameObject.Find("玩家").transform;
+    }
+
+    //在Update後執行 : 適合做攝影機或物件的座標追蹤
+    private void LateUpdate()
+    {
+        Track();
+    }
+
+    /// <summary>
+    /// 攝影機追蹤
+    /// </summary>
+    private void Track()
+    { 
+        Vector3 posPlayer = player.position;
+        Vector3 posCamera = transform.position;
+
+        posPlayer.x = 0;   //限制x在0
+        posPlayer.y = 16;  //限制y在16(原攝影機)
+        posPlayer.z -= 15; //往玩家後面移動
+
+        posPlayer.z = Mathf.Clamp(posPlayer.z, top, bottom);   //利用Clamp讓攝影機不會超過上下限
+
+        transform.position = Vector3.Lerp(posCamera, posPlayer, 0.5f * Time.deltaTime * speed);
+    }
+}
---
+++ UnityProject_3DArrow/Assets/Scripts/LevelManager.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class LevelManager : MonoBehaviour
+{
+    [Header("是否顯示隨機技能")]
+    public bool showRandomSkill;
+    [Header("是否直接開門")]
+    public bool autoOpenDoor;
+    [Header("隨機技能選單")]
+    public GameObject randomSkill;
+
+    private Animator door;
+
+    private void Start()
+    {
+        door = GameObject.Find("門").GetComponent<Animator>();
+
+        if (autoOpenDoor) Invoke("OpenDoor", 8);    //Invoke -> 延遲調用方法
+        if (showRandomSkill) ShowRandomSkill();
+    }
+
+    /// <summary>
+    /// 顯示隨機技能畫面
+    /// </summary>
+    private void ShowRandomSkill()
+    {
+        randomSkill.SetActive(true);
+    }
+
+    /// <summary>
+    /// 播放開門動畫
+    /// </summary>
+    private void OpenDoor()
+    {
+        door.SetTrigger("開門");
+    }
+}
